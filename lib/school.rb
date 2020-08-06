0e23e47c926e6d33142cac031603243af27982e0
Megan (AKA Pilahi) Moran <kamokumoran@gmail.com>
2020-08-06 04:46:21 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -11,11 +11,18 @@ class School
     roster[grade] << student
   end
   
-  def sort 
-  nu_hash = {}
-  roster.each do |x, y| 
-    nu_hash[x] = y.sort 
+  def grade(level)
+    roster.detect do |x, y| 
+      if x == level
+        return y 
+      end 
   end 
+    
+  def sort 
+    nu_hash = {}
+    roster.each do |x, y| 
+      nu_hash[x] = y.sort 
+    end 
   nu_hash
-end 
+  end 
 end
