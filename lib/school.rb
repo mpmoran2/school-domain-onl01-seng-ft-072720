90406f9d53785921c28a6ea3237ee0302e48cf36
Megan (AKA Pilahi) Moran <kamokumoran@gmail.com>
2020-08-06 04:43:49 +0000

    Automatically backed up by Learn

---
+++ lib/school.rb
@@ -0,0 +1,20 @@
+class School
+  attr_accessor :name, :roster
+  
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+  
+  def add_student(student, grade)
+    roster[grade] ||= []
+    roster[grade] << student
+  end
+  
+  def grade(grade)
+    roster.detect do |x,y|
+      if x == grade
+        return y
+      end
+    end
+end
